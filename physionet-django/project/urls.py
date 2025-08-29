674df525d0e2cb5b9465345f6106822c95f40490
Tom Pollard <tpollard@mit.edu>
2025-08-28 22:32:59 -0400

    Add upload agreement path.

--- physionet-django/project/urls.py
+++ physionet-django/project/urls.py
@@ -72,6 +72,7 @@ urlpatterns = [
     path('<project_slug>/submission/', views.project_submission, name='project_submission'),
     path('<project_slug>/ethics/', views.project_ethics, name='project_ethics'),
     path('<project_slug>/ethics/edit-document/', views.edit_ethics, name='edit_ethics'),
+    path('<project_slug>/upload-agreement/', views.project_upload_agreement, name='project_upload_agreement'),
     path('ethics/<path:file_name>/', views.serve_document, name='serve_document'),
     path(
         '<project_slug>/view-required-trainings/',
